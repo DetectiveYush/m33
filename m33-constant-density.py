b79747013012a1809ab2ea242e11da91bc46d20a
Detective <detective@pop-os.localdomain>
2023-09-25 17:16:08 -0400

    Added constant density dark matter model

--- m33-constant-density.py
+++ m33-constant-density.py
@@ -15,8 +15,10 @@ m=m2
 #print (m1)
 vol=(4/3)*math.pi*(rm**3)
 d=m/vol
+dd=(m3-m)/vol
 
 v=[]
+vd=[]
 rtest=[1e3,3e3,5e3,8e3, 10e3, 20e3,30e3,40e3,50e3]
 r=np.linspace(1e3,50e3,100)
 
@@ -25,18 +27,34 @@ for i in r:
     if i<=rm:
         mi=(4/3)*math.pi*(i**3)*d
     else:
-        mi=m2
+        mi=m
+    md=(4/3)*math.pi*(i**3)*dd
     #print("r=",i,",m=",mi)
     vi=math.sqrt(g*(mi)/(k*i))
     v.append(vi)
+    vdi=math.sqrt((vi**2)+(g*md/i))
+    vd.append(vdi)
 
 
 #print("r=",r)
 #print("v=",v)
+
+#Now we'll model with Dark matter (constant density)
+
+#vd=math.sqrt((v**2)+(gm3/r))
 
 plt.plot(r, v)
 plt.title("Approximated classical m33 rotation curve")
 plt.xlabel("Distance from galaxy center (ly)")
 plt.ylabel("Velocity of star (Km/s)")
 plt.savefig("rotation_curve.png")
+#plt.show()
+
+plt.plot(r, vd)
+plt.title("Approximated m33 rotation curve with Dark matter")
+plt.xlabel("Distance from galaxy center (ly)")
+plt.ylabel("Velocity of star (Km/s)")
+plt.savefig("rotation_curve_dark_matter.png")
 plt.show()
+
+
